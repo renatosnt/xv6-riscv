3ad7f117a1d68efca030f6f829137d19e87a7212
Renato <renatossnt@gmail.com>
2024-07-13 22:14:47 -0300

    tp2 completo

--- user/getpinfo.c
+++ user/getpinfo.c
@@ -3,90 +3,77 @@
 #include "user/user.h"
 #include "kernel/pstat.h"
 
-#define NCHILDS 3
 
 int main(int argc, char *argv[])
 {
-	fprintf(1, "calling getpinfo...\n");
-	settickets(29);
+    printf("Calling getpinfo...\n");
+    settickets(29);
 
-	if (argc != 2)
-	{
-		fprintf(2, "USAGE: %s <rows_csv_file>\n", argv[0]);
-		exit(-1);
-	}
+    if (argc != 2)
+    {
+        fprintf(2, "USAGE: %s <num_rows>\n", argv[0]);
+        exit(-1);
+    }
 
-	const int NROWS = atoi(argv[1]);
+    int numRows = atoi(argv[1]);
+    int childPIDs[3];
 
-	int pids[NCHILDS];
 
-	for (int i = 0; i < NCHILDS; i++)
-	{
-		switch (pids[i] = fork())
-		{
-		case -1: /* fork() fallÃ³ */
-			fprintf(1, "perror(fork");
-			exit(-1);
-			break;
-		case 0: /* Ejecucion del proceso hijo tras fork() con Ã©xito */
-			settickets(10 * (i + 1));
-			int b = 0;
-			for (int i = 0; i < 9999999990; i++)
-			{
-				b = 40 * 40 + i;
-			}
-			fprintf(1, "%d\n", b);
-			exit(0);
-			break;
-		default:
-			break;
-		}
-	}
+    for (int i = 0; i < 3; i++)
+    {
+        int pid = fork();
+        if (pid < 0) 
+        {
+            fprintf(2, "Error: fork failed\n");
+            exit(-1);
+        }
+        else if (pid == 0) 
+        {
+            settickets(10 * (i + 1));
+            int b = 0;
+            for (int j = 0; j < 9999999990; j++)
+            {
+                b = 40 * 40 + j;
+            }
+            printf("%d\n", b);
+            exit(0);
+        }
+        else 
+        {
+            childPIDs[i] = pid;
+        }
+    }
+    printf("PID\tTICKETS\tTICKS\n");
 
-	int fd = open("salida.csv", 0x200 | 0x001); // O_CREATE | O_WRONLY
-	if (fd < 0)
-	{
-		fprintf(2, "Cannot open file!.");
-		exit(-1);
-	}
+    for (int row = 0; row < numRows; row++)
+    {
+        struct pstat ps;
+        int result = getpinfo(&ps);
+        if (result < 0)
+        {
+            fprintf(2, "Error: getpinfo failed\n");
+            exit(1);
+        }
+        printf("\n");
+        for (int i = 0; i < NPROC; i++)
+        {
+            if (ps.inuse[i])
+            {
+                if (ps.tickets[i] % 10 == 0)
+                {
+                    printf("%d\t%d\t%d\n", ps.pid[i], ps.tickets[i], ps.ticks[i]);
+                }
+            }
+        }
 
-	fprintf(fd, "Process A;Process B;Process C\n");
-	
-	for (int j = 0; j < NROWS; j++)
-	{
-		fprintf(1, "\n");
-		struct pstat ps;
-		int ret = getpinfo(&ps);
-		if (ret < 0)
-		{
-			fprintf(2, "getpinfo error\n");
-			exit(1);
-		}
-        fprintf(1, "PID\tTICKETS\tTICKS\n");
-		for (int i = 0; i < NPROC; i++)
-		{
-			if (ps.inuse[i])
-			{
-				char endl;
-				if (ps.tickets[i] == 30)
-					endl = '\n';
-				else
-					endl = ';';
-				if (ps.tickets[i] % 10 == 0)
-				{
-					fprintf(1, "%d\t%d\t%d\n", ps.pid[i], ps.tickets[i], ps.ticks[i]);
-					fprintf(fd, "%d%c", ps.ticks[i], endl);
-				}
-			}
-		}
+        sleep(100); 
+    }
 
-		sleep(100);		
-	}
+    for (int i = 0; i < 3; i++)
+    {
+        kill(childPIDs[i]);
+    }
 
-	for (int i = 0; i < NCHILDS; i++)
-		kill(pids[i]);
-
-	fprintf(1, "Returning from getinfo.c with status 0!\n");
-
-	exit(0);
+    printf("Returning from getpinfo test with status 0!\n");
+    exit(0);
 }
